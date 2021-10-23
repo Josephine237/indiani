bb7af1a18943aa7f7cd2543c90840194aa030d2e
Your Name <you@example.com>
2021-10-23 21:02:26 +0200

    prvni commit indianu

---
+++ indians-moje.js
@@ -0,0 +1,31 @@
+
+
+for (let i = 1; i <= 10; i++) {
+    if (i === 3) {
+        console.log(i = 3, "little indians\n")
+    } else if (i === 6) {
+        console.log(i = 6, "little indians\n")
+    } else if (i === 9) {
+        console.log(i = 9, "little indians\n")
+    } else if (i === 10) {
+        console.log(i = 10, "little indian Boys\n")
+    } else {
+        console.log(i, "little")
+    }
+}
+
+
+for (let i = 10; i > 0; i--) {
+    if (i === 8) {
+        console.log(i = 8, "little indians\n")
+    } else if (i === 5) {
+        console.log(i = 5, "little indians\n")
+    } else if (i === 2) {
+        console.log(i = 2, "little indians\n")
+    } else if (i === 1) {
+        console.log(i = 1, "little indian Boy\n")
+    } else {
+        console.log(i, "little")
+    }
+
+}
